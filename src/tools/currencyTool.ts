b3b5d75c3db2213ae4b2a245ba35030a830729e7
Yevgenii Mikhnytskyi <todesstoss@gmail.com>
2025-05-30 21:52:07 +0300

    fmt fix

--- src/tools/currencyTool.ts
+++ src/tools/currencyTool.ts
@@ -1,5 +1,6 @@
 import type { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { getCurrencies } from "../services/currency.ts";
+
 export const initCurrencyTool = (server: McpServer) => {
   server.tool(
     "monobank-currency",
