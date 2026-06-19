3acdcd9bfdca7dd1589de9196ca2fdf4090feaf5
Yevgenii Mikhnytskyi <todesstoss@gmail.com>
2026-06-20 01:20:12 +0300

    simplify tool registration with array pattern

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,14 @@ import { initStatementTool } from "./tools/statementTool.ts";
 import { initAccountsTool } from "./tools/accountsTool.ts";
 import { initWebhookTool } from "./tools/webhookTool.ts";
 
-initCurrencyTool(server);
-initClientInfoTool(server);
-initStatementTool(server);
-initAccountsTool(server);
-initWebhookTool(server);
+const tools = [
+  initCurrencyTool,
+  initClientInfoTool,
+  initStatementTool,
+  initAccountsTool,
+  initWebhookTool,
+];
+
+tools.forEach((init) => init(server));
 
 await server.connect(transport);
